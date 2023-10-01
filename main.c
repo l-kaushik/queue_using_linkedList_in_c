af09ce6c04c11e58b5b75ee878af30dbdd4cec19
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-01 09:14:15 +0530

    Structure generation function 1. createQueue() will allocate memory fo Queue and for one node too. 2. checkNull() will check for NULL pointers.

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void checkNull(void *ptr){
+    if(NULL == ptr){
+        puts("Memory not allocated");
+        exit(EXIT_FAILURE);
+    }
+}
 
 typedef struct Node {
     int data;
@@ -10,6 +18,16 @@ typedef struct Queue {
     Node *front;
     Node *rear;
 } Queue;
+
+Queue * createQueue(){
+    Queue *q = (Queue *)malloc(sizeof(Queue));
+    checkNull(q);
+    Node *n = (Node *)malloc(sizeof(Node));
+    checkNull(n);
+    n->next = NULL;
+
+    return q;
+}
 
 int main()
 {
